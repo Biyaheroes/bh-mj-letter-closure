d7a9162d4f41956e2292223ad8c69e5dc5b87768
volkovasystems <richeve.bebedor@gmail.com>
2017-07-03 00:14:51 +0800

    Finished bh-mj-letter-closure component.

--- letter-closure.jsx
+++ letter-closure.jsx
@@ -36,8 +36,8 @@
 			"author": "Biyaheroes Developers",
 			"contributors": [
 				"Robot Biyaheroes <robot@biyaheroes.com>",
-				"Richeve S. Bebedor <richeve.bebedor@gmail.com>",
-				"Vinse Vinalon <vinsevinalon@gmail.com>"
+				"Vinse Vinalon <vinsevinalon@gmail.com>",
+				"Richeve S. Bebedor <richeve.bebedor@gmail.com>"
 			],
 			"eMail": "developers@biyaheroes.com",
 			"repository": "https://github.com/Biyaheroes/bh-mj-letter-closure.git",
@@ -94,15 +94,12 @@ class LetterClosure extends Component {
 
 		return ( <Section
 					{ ...this.props }
-					padding="10px 0px 40px 0px"
 				>
 					<Column>
 							<Text
-								style={ {
-									"padding": "0px 30px 0px 30px",
-									"fontSize": "15px",
-									"letterSpacing": "0.5px",
-								} }
+								padding="10px 30px 40px 30px"
+								font-size="15px"
+								letter-spacing="0.5px"
 							>
 								{ paragraph }
 							</Text>
